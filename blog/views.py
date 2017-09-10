87e30ec71af0d3a0206748f6e969d9f6ce952aab
Vishnu Priya Nallan Chakravarthula <VishnuPriya.NallanChakravarthula@jda.com>
2017-09-10 18:20:22 +0530

    Added table_form2.html with safe attribute for help_text so that django default password displays correctly.

--- blog/views.py
+++ blog/views.py
@@ -91,7 +91,7 @@ def comment_remove(request,pk):
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
